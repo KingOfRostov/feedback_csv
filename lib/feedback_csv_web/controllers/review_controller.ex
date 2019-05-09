dd3f6df825e376b532b80d147659069ea54e5cf1
Sergey <mk-serg@yandex.ru>
2019-05-09 16:27:43 +0300

    Loading info from CSV files to DB
    Ignores date field. Need to be fixed.

--- lib/feedback_csv_web/controllers/review_controller.ex
+++ lib/feedback_csv_web/controllers/review_controller.ex
@@ -2,6 +2,7 @@ defmodule FeedbackCsvWeb.ReviewController do
   use FeedbackCsvWeb, :controller
 
   alias FeedbackCsv.Reviews.Review
+  alias FeedbackCsv.Reviews
 
   def index(conn, _params) do
     reviews = FeedbackCsv.Reviews.list_review()
@@ -11,21 +12,31 @@ defmodule FeedbackCsvWeb.ReviewController do
 
   # Если выбран какой-либо файл
   def create(conn, %{"review" => review_params}) do
-    reviews = FeedbackCsv.Reviews.list_review()
-    changeset = Review.changeset(%Review{}, %{})
-
     upload = review_params["csv"]
     extension = Path.extname(upload.filename)
 
     # Если загружен .csv файл - добавляем в ./media, иначе не добавляем 
     if(extension == ".csv") do
       {:ok, time} = DateTime.now("Etc/UTC")
+      # Генерируем имя для файла
       formated_time = String.replace(to_string(time), " ", "_")
-      File.cp(upload.path, "./media/#{formated_time}#{extension}")
+      formated_name = "./media/#{formated_time}#{extension}"
+      # Переносим файлы в папку ./media
+      File.cp(upload.path, formated_name)
 
-      put_flash(conn, :info, "Данные успешно обработаны")
+      # Загружаем данные из csv файла в БД, а затем удаляем файл
+      Reviews.csv_to_db(formated_name)
+      File.rm(formated_name)
+
+      reviews = FeedbackCsv.Reviews.list_review()
+      changeset = Review.changeset(%Review{}, %{})
+
+      put_flash(conn, :info, "Данные успешно загружены")
       |> render("index.html", %{reviews: reviews, changeset: changeset})
     else
+      reviews = FeedbackCsv.Reviews.list_review()
+      changeset = Review.changeset(%Review{}, %{})
+
       put_flash(conn, :error, "Принимаются только .csv файлы")
       |> render("index.html", %{reviews: reviews, changeset: changeset})
     end
