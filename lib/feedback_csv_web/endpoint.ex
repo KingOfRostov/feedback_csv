316a545c65890ead2ed99827722a3e136e12c3bf
Sergey <mk-serg@yandex.ru>
2019-05-31 21:31:23 +0300

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/feedback_csv_web/endpoint.ex
+++ lib/feedback_csv_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule FeedbackCsvWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :feedback_csv
 
   socket "/socket", FeedbackCsvWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
